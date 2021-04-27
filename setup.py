098374dad18b0b0ce1fe87dea0848243ba7f7c62
donaldkelly17 <donaldkelly17@gmail.com>
2021-04-27 11:00:14 +0100

    added setup files and licence text

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dsnd_probability_dee_version",
+    version="0.1",
+    description="Gaussian and Binomial distributions",
+    packages=["dsnd_probability_dee_version"],
+    author="Donald Kelly",
+    author_email="myemail017@gmail.com",
+    zip_safe=False,
+)
